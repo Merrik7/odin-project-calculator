f72f07aeb7c3569b55529ca01cef5ea14fc4b62f
Merrik7 <73802737+Merrik7@users.noreply.github.com>
2024-04-02 15:57:05 -0500

    part 4

---
+++ script.js
@@ -0,0 +1,29 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const operate = function (operator, a, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else {
+    return `error`;
+  }
+};
