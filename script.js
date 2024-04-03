bf0ce56ee06c6e263e60e1462525881d1097c024
Merrik7 <73802737+Merrik7@users.noreply.github.com>
2024-04-03 11:34:19 -0500

    calculator finished

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-const calculatorBtns = document.querySelectorAll("calculator-btn");
+'use strict';
 
-const add = function (a, b) {
-  return a + b;
-};
+const calculatorBtns = document.querySelectorAll('.calculator-btn');
+const calculatorText = document.querySelector('.calculator-text');
+let calculation = '';
 
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-const operate = function (operator, a, b) {
-  if (operator === "+") {
-    return add(a, b);
-  } else if (operator === "-") {
-    return subtract(a, b);
-  } else if (operator === "*") {
-    return multiply(a, b);
-  } else if (operator === "/") {
-    return divide(a, b);
-  } else {
-    return `error`;
+const operate = function (calculation) {
+  const numbersToCalc = calculation.split(' ');
+  let total = Number(numbersToCalc[0]);
+  for (let i = 0; i < numbersToCalc.length - 1; i++) {
+    if (Number(numbersToCalc[i])) {
+      if (numbersToCalc[i + 1] === '+') {
+        total += total + Number(numbersToCalc[i + 2]);
+      } else if (numbersToCalc[i + 1] === '-') {
+        total -= Number(numbersToCalc[i + 2]);
+      } else if (numbersToCalc[i + 1] === '*') {
+        total *= Number(numbersToCalc[i + 2]);
+      } else if (numbersToCalc[i + 1] === '/') {
+        total /= Number(numbersToCalc[i + 2]);
+      } else {
+        total += Number(numbersToCalc[i]);
+      }
+    }
   }
+  return total;
 };
 
-for (let i = 0; i < calculatorBtns.length; i++) {}
+let nextNum = true;
+
+for (let i = 0; i < calculatorBtns.length; i++) {
+  calculatorBtns[i].addEventListener('click', function (event, listener) {
+    console.log(calculatorBtns[i].classList[1]);
+
+    let subbedString = calculatorBtns[i].classList[1].replace('num-', '');
+    let operator = '';
+
+    if (Number(subbedString)) {
+      console.log(nextNum);
+      calculation += subbedString;
+      calculatorText.textContent = calculation;
+      nextNum = false;
+    } else if (subbedString === 'clear') {
+      calculation = '';
+      calculatorText.textContent = calculation;
+      nextNum = true;
+    } else if (subbedString === 'equals') {
+      calculatorText.textContent = operate(calculation);
+      nextNum = true;
+      calculation = '';
+    } else {
+      operator = subbedString;
+      if (operator === 'plus' && nextNum === false) {
+        calculation += ' + ';
+        calculatorText.textContent = calculation;
+        nextNum = true;
+      } else if (operator === 'minus' && nextNum === false) {
+        calculation += ' - ';
+        calculatorText.textContent = calculation;
+        nextNum = true;
+      } else if (operator === 'multiply' && nextNum === false) {
+        calculation += ' * ';
+        calculatorText.textContent = calculation;
+        nextNum = true;
+      } else if (operator === 'divide' && nextNum === false) {
+        calculation += ' / ';
+        calculatorText.textContent = calculation;
+        nextNum = true;
+      }
+    }
+  });
+}
